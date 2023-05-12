66e314f12a9bf842d353178a13e7c151f032b56a
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-12 11:50:37 -0400

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,16 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
 const Footer = () => {
-  return <div>This is Footer</div>;
+  return (
+    <div className="--footer">
+      <a href="https://github.com/jayhons99">
+        <GitHubIcon className="--footer-icon" fontSize="large" />
+      </a>
+      <InstagramIcon className="--footer-icon" fontSize="large" />
+      <TwitterIcon className="--footer-icon" fontSize="large" />
+    </div>
+  );
 };
 export default Footer;
