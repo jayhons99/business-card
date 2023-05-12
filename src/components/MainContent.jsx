cb256dbe9710e68a6a38f84c124cb58173f79d87
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-12 11:16:36 -0400

    added main content styling and media responsiveness

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,4 +1,21 @@
 const MainContent = () => {
-  return <div>This is MainContent</div>;
+  return (
+    <div className="--main">
+      <h3 className="--main-header">About</h3>
+      <p className="--main-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum
+        sodales blandit. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Nunc sit amet feugiat lorem, nec
+        vestibulum mauris.
+      </p>
+      <h3 className="--main-header">Hobbies</h3>
+      <p className="--main-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum
+        sodales blandit. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Nunc sit amet feugiat lorem, nec
+        vestibulum mauris.
+      </p>
+    </div>
+  );
 };
 export default MainContent;
