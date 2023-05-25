114f862812bac973b8c4eebac13f820513f5fbf9
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-05-25 10:49:51 -0400

    added stats to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import Card from "./components/Card";
+import { useEffect, useState } from "react";
+
+const prodUrl = "https://tiijwog187.execute-api.us-east-1.amazonaws.com/Prod/";
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    fetch(prodUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setCount(data.numOfVisits);
+      });
+  }, []);
   return (
     <>
-      <Card />
+      <Card count={count} />
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,18 @@
+/* eslint-disable react/prop-types */
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
+import logo from "/aws-logo.png";
 
-const Card = () => {
+const Card = ({ count }) => {
   return (
     <div className="--card">
       <Header />
       <MainContent />
+      <h1 className="aws-stats"># of visitors: {count}</h1>
+      <p className="aws-stats p">
+        Powered by <img className="logo" src={logo}></img>
+      </p>
       <Footer />
     </div>
   );
